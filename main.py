dca068f672f1f731c44443de179b05633774bbd1
Olehpt <papinchakolehh@gmail.com>
2026-02-13 14:35:14 +0200

    test commit 2

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('test gui')
-        self.setGeometry(100,100, 1280, 720)
-
-def main():
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
-
-#test
-if __name__ == '__main__':
-    main()
+#nothing
