4c7413cc991c258c85a7f3103cd313d5f75ff272
Olehpt <papinchakolehh@gmail.com>
2026-02-13 13:49:33 +0200

    test commit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle('test gui')
+        self.setGeometry(100,100, 1280, 720)
 
 def main():
     app = QApplication(sys.argv)
