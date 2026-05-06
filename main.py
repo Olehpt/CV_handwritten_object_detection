d8d20167086f6ff200a407d16c15b36a25a92cbb
Olehpt <papinchakolehh@gmail.com>
2026-05-06 12:08:21 +0300

    Refactored contour detection sample, deleted unused test samples, added semi-final contour reading into main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-#nothing
+import cv2 as cv, os, numpy as np
+from src.contourDetection import contour_detection
+
+file_paths = ['file1', 'file2', 'file3']
+
+imgs = []
+for path in file_paths:
+    img = cv.imread(path)
+    if img is not None:
+        imgs.append(img)
+        print(f"Image {path} added")
+
+contours = []
+for img in imgs:
+    contour = contour_detection(img)
+    if not contour.empty(): contours.append(contour)
+    else : print(f'Empty contour from {img.path}')
---
+++ src/contourDetection.py
@@ -0,0 +1,15 @@
+import cv2
+
+def contour_detection(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(
+        thresh,
+        cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_NONE
+    )
+    contour = max(contours, key=cv2.contourArea)
+    epsilon = 2.0
+    approx = cv2.approxPolyDP(contour, epsilon, True)
+    points = approx.reshape(-1, 2)
+    return points
