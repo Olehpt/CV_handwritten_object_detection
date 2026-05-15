e974ab03834597748b6e3fb7e9545aa6a9483ae6
Olehpt <papinchakolehh@gmail.com>
2026-05-15 19:19:52 +0300

    Added sample for auto cropping & resizing

--- src/recon_test.py
+++ src/recon_test.py
@@ -1,12 +1,49 @@
-import draw, geometry, contourDetection
+import draw, contourDetection
 import numpy as np
 import cv2
+from geometry import Point
+import math
 
-img = cv2.imread("../data/test3.png")
+img = cv2.imread("../data/test2.png")
 points = contourDetection.contour_detection(img)
+vox_resolution = 128
 
+#cropping
+centroid = Point(0,0)
 for point in points:
-    draw.draw_dot_bounds(img, geometry.Point(point[0], point[1]), 5, (0, 255, 0))
+    centroid.x += point[0]
+    centroid.y += point[1]
+centroid.x = centroid.x // len(points)
+centroid.y = centroid.y // len(points)
+print(centroid)
+draw.draw_dot_bounds(img, centroid, 5, (0,255,0))
 
-grid_size = 128
-voxel_grid = np.zeros((grid_size, grid_size, grid_size), dtype = bool)
+p1 = Point(centroid.x, centroid.y)#left
+p2 = Point(centroid.x, centroid.y)#top
+p3 = Point(centroid.x, centroid.y)#righd
+p4 = Point(centroid.x, centroid.y)#bottom
+for point in points:
+        x, y = point
+        draw.draw_dot_bounds(img, Point(x, y), 5, (255,0,0))
+        if x < p1.x: p1 = Point(x,y)
+        if y < p2.y: p2 = Point(x,y)
+        if x > p3.x: p3 = Point(x,y)
+        if y > p4.y: p4 = Point(x,y)
+
+p_low = Point(p1.x, p4.y)
+p_high = Point(p3.x, p2.y)
+
+
+
+
+
+draw.draw_dot_bounds(img, p1, 5, (0,0,255))
+draw.draw_dot_bounds(img, p2, 5, (0,0,255))
+draw.draw_dot_bounds(img, p3, 5, (0,0,255))
+draw.draw_dot_bounds(img, p4, 5, (0,0,255))
+draw.draw_dot_bounds(img, p_low, 5, (0,255,0))
+draw.draw_dot_bounds(img, p_high, 5, (0,255,0))
+
+cv2.imshow("img", img)
+cv2.waitKey(0)
+
